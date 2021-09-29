71bd9506541993dba0b7244d991583d15e924a3c
Rebecca Vest <rvest@equinoxmedia.com>
2021-09-28 18:26:49 -0600

    Add CodeDeploy/CodeBuild/CodePipeline

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -10,6 +10,11 @@ AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
   --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
 
+# Generate a personal access token with repo and admin:repo_hook
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+
 # Deploy the CloudFormation template
 
 echo -e "\n\n========= Deploying setup.yml =========="
@@ -23,13 +28,18 @@ aws cloudformation deploy \
 --parameter-overrides \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
-echo -e "\n\n========= Deploying main.yml =========="
-aws cloudformation deploy \
---region $REGION \
---profile $CLI_PROFILE \
---stack-name $STACK_NAME \
---template-file main.yml \
---no-fail-on-empty-changeset \
---capabilities CAPABILITY_NAMED_IAM \
---parameter-overrides \
-    EC2InstanceType=$EC2_INSTANCE_TYPE
+# echo -e "\n\n========= Deploying main.yml =========="
+# aws cloudformation deploy \
+# --region $REGION \
+# --profile $CLI_PROFILE \
+# --stack-name $STACK_NAME \
+# --template-file main.yml \
+# --no-fail-on-empty-changeset \
+# --capabilities CAPABILITY_NAMED_IAM \
+# --parameter-overrides \
+#     EC2InstanceType=$EC2_INSTANCE_TYPE \
+#     GitHubOwner=$GH_OWNER \
+#     GitHubRepo=$GH_REPO \
+#     GitHubBranch=$GH_BRANCH \
+#     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+#     CodePipelineBucket=$CODEPIPELINE_BUCKET
